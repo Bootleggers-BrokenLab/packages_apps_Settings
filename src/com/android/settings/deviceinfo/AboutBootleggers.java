d308aaad0691291efd6477d36dccf8248896f58e
tribual <tribual@web.de>
2024-10-25 11:09:43 +0200

    Settings: rework About page and ad BTLG Logo

--- src/com/android/settings/deviceinfo/AboutBootleggers.java
+++ src/com/android/settings/deviceinfo/AboutBootleggers.java
@@ -62,49 +62,6 @@ public class AboutBootleggers extends DashboardFragment {
 
         // starting from 10 (inclusive) and end at order 100 at max
         int order = 10;
-
-        // adding maintainers
-        final String maintainers[] = res.getString(R.string.maintainer_name).split(",");
-        final String maintainerLinks[] = res.getString(R.string.maintainer_telegram).split(",");
-        for (int i = 0; i < maintainers.length && order < 100; i++) {
-            final String name = maintainers[i];
-            final String link = i < maintainerLinks.length ? maintainerLinks[i] : "";
-            if (name == null || name.isEmpty())
-                continue;
-            addMaintainerPref(name, link, order++);
-        }
-
-        // adding kernel devs
-        final String kernels[] = res.getString(R.string.kernel_name).split(",");
-        final String kernelLinks[] = res.getString(R.string.kernel_telegram).split(",");
-        for (int i = 0; i < kernels.length && order < 100; i++) {
-            final String name = kernels[i];
-            final String link = i < kernelLinks.length ? kernelLinks[i] : "";
-            if (name == null || name.isEmpty())
-                continue;
-            addMaintainerPref(name, link, true, order++);
-        }
-    }
-
-    private void addMaintainerPref(String name, String link, int order) {
-        addMaintainerPref(name, link, false, order);
-    }
-
-    private void addMaintainerPref(String name, String link, boolean kernel, int order) {
-        final String sum = getResources().getString(
-                kernel ? R.string.kernel : R.string.maintainer);
-        Preference pref = new Preference(getContext());
-        pref.setTitle(name);
-        pref.setSummary(sum);
-        pref.setOrder(order);
-        pref.setCopyingEnabled(false);
-        if (link != null && !link.isEmpty() &&
-                URLUtil.isValidUrl(link) && URLUtil.isHttpsUrl(link)) {
-            final Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse(link));
-            pref.setIntent(intent);
-        }
-        getPreferenceScreen().addPreference(pref);
     }
 
     @Override
